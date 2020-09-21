a9818f73c810f5a17ed751c42f2ba3afde3050d8
Bilal <bbilalkaya@gmail.com>
2020-09-21 16:23:46 +0300

    yaml

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import menu from "./menu.yaml";
 
 function App() {
-  return <div className="App"></div>;
+  console.log(menu.menus);
+  return (
+    <div className="App">
+      {menu.menus.map((item, index) => (
+        <div key={index}>{item.key}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
